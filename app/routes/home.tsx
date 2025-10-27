a92a61b484cfd9ca106d493ab6ad404495b9e508
Mika <40500858+EinHecki@users.noreply.github.com>
2025-10-27 18:04:21 +0100

    Remove hobex logo

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -285,7 +285,6 @@ export default function Home() {
             logos={[
                 "pc-cash_logo.png",
                 "digituu_logo.png",
-                "hobex_logo.png",
             ]}
         />
 
