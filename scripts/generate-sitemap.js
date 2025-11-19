c1182a99057aff816aa8f82cd31f4c7f79835441
Mika <40500858+EinHecki@users.noreply.github.com>
2025-11-19 18:55:21 +0100

    Fix sitemap tld

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const baseUrl = "https://byteserv.de";
+const baseUrl = "https://byteserv.it";
 const today = new Date().toISOString().split('T')[0];
 
 const routes = [
